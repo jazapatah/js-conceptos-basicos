edbae983e868c1fd12762f14cc47c1848f0b7369
Jonatan <jotan.alexander@gmail.com>
2023-07-12 23:07:04 -0500

    variables

--- js/variables.js
+++ js/variables.js
@@ -1,1 +1,21 @@
+// para declarar variables se utilizan var, let y const
+//Mejor no usar var, puede ocasionar una serie de inconvenientes
+
+/** 
+ * Tipos de datos son: string (textos), number,
+ *  boolean (false, true), symbol, object, null
+ * y undefined
+ */ 
+
+let edad = 20; //Tipo number
+
+const miNombre = 'Jorge'; //Tipo string
+
+const casado = false; //Tipo boolean
+
+/* Cuando declaro una variable con let se puede modificar, pero si la declaro con const, no puedo hacerlo */
+
+edad = 25;
+
+console.log(edad)
 // min 31:00
