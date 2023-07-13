92f1a227187f4e092296c69957530c7fb4700784
Jonatan <jotan.alexander@gmail.com>
2023-07-12 19:45:12 -0500

    first commit

---
+++ js/intro.js
@@ -0,0 +1,11 @@
+//comentario de línea
+/* o multilinea o de párrafo */
+
+// Crear la app de Hola Mundo
+
+/**
+ * @return {String} Holan mundo es una cadena de texto 
+*/ 
+
+// Imprime en la consola del navegador
+console.log('Hola Mundo')
