36f4964743d7e0fb9e7feb5f10aa29b18307ad03
Jonatan <jotan.alexander@gmail.com>
2023-07-19 18:53:46 -0500

    objetos-literales

--- js/objetos-literales.js
+++ js/objetos-literales.js
@@ -17,4 +17,40 @@ console.warn('Mostrar el nombre del auto');
 console.log('Nombre:', auto.nombre); // nombre del objeto.propiedad del objeto
 console.log('Precio y el color:', auto['precio'], auto['color']); //Otra forma de mostrar información del objeto
 
-//min 2:05:37 
+// Crear una nueva propiedad para el objeto
+auto.tipoAuto = 'Familiar'; // camelCase
+auto.tipoAuto1 = 'Familiar-GamaBaja'; // camelCase
+auto.tipoAuto2 = 'Familiar-GamaAlta'; // camelCase
+console.log(auto);
+
+// Eliminar una propiedad del objeto
+delete auto.tipoAuto;
+console.log(auto);
+
+// Crear un objeto para determinar las propiedades de una película
+
+let pelicula = {
+    nombre:'Indiana Jones 5',
+    actor1: 'Harrison Ford',
+    actor2: 'Antonio Banderas',
+    actor3: 'Phoebe Waller-Bridge',
+    actor4: 'Mads Mikkelsen',
+    estreno: '29 de Junio de 2023',
+    director: 'James Mangold',
+    presupuesto:300000000,
+    // Anidación de objeto, con arreglo
+    actores: ['Harrison Ford', 'Antonio Banderas', 'Phoebe Waller-Bridge', 'Mads Mikkelsen'],
+};
+
+console.warn('Película');
+console.table(pelicula);
+
+// Cuántos actores hay en el objeto de película
+console.log('Los actores son: ', pelicula.actores.length); // .lenght, da el número de los elementos de un arreglo
+
+// Anexar una propiedad para el objeto pelicula, en que salas
+pelicula.salas = ['Viva', 'Mayorca', 'Florida', 'Los Puentes'];
+
+console.log(pelicula)
+
+//min 2:40:24 
