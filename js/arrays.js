2bd9ad07cf8fdb22beac38c71640f1aec79e3f49
Jonatan <jotan.alexander@gmail.com>
2023-07-19 19:20:25 -0500

    starts arrays

---
+++ js/arrays.js
@@ -0,0 +1,7 @@
+// Crear el arreglo para las marcas de autos
+let marcasAutos = ['Ferrari', 'Mazda', 'Toyota', 'Audi', 'Renault'];
+
+console.log(marcasAutos);
+console.log(marcasAutos[2]); // Mostrar Toyota
+
+// min 
